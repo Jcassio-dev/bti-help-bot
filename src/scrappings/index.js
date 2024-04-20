f83d1b02f37ad914f142c78ea10cc212cdcc11af
André Freitas <freitasandre38@gmail.com>
2024-04-20 15:37:32 -0300

    refactor: remake make scrapping function into src > scsrappings > index.js. use all settled instead axios await

--- src/scrappings/index.js
+++ src/scrappings/index.js
@@ -5,9 +5,19 @@ const fetchJobs = require("./jerimumjobs");
 async function makeScrapping() {
   console.log("Iniciando scrapping...");
 
-  await fetchEditais().then((editais) => (global.appContext.editais = editais));
-  await fetchMenuRU().then((menu) => (global.appContext.ruCardapio = menu));
-  await fetchJobs().then((jobs) => (global.appContext.jobs = jobs));
+  const scrapingPromises = [fetchEditais(), fetchMenuRU(), fetchJobs()];
+
+  // Leia mais sobre allSettled 
+  const results = await Promise.allSettled(scrapingPromises);
+
+  // Diferentemente do Promise.all, o allSettled armazena os resultados. Se necessário, crie uma função extra para validar.
+  results.forEach((result, index) => {
+    if (result.status === "fulfilled") {
+      global.appContext[index === 0 ? "editais" : index === 1 ? "ruCardapio" : "jobs"] = result.value;
+    } else {
+      console.error(`Error fetching data for ${index === 0 ? "editais" : index === 1 ? "menuRU" : "jobs"}:`, result.reason);
+    }
+  });
 
   console.log("Scrapping finalizado!");
 }
