d62fdb456767e874afb9b4be3ffd341744a154d0
jose cassio <josecassio2013@gmail.com>
2024-04-20 15:00:16 -0300

    feat: block replies in groups on dev mode

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -1,13 +1,21 @@
-const commands = require('./commands');
+const commands = require("./commands");
 
 module.exports = (client) => {
-    client.on('message', async (msg) => {
-        const {body} = msg;
-        const normalizedBody = body.split(" ")[0]?.toString().toLowerCase();
-        const command = commands[normalizedBody];
+  client.on("message", async (msg) => {
+    if (process.env.NODE_ENV === "development" && msg.from.endsWith("@g.us")) {
+      return;
+    }
 
-        body === '!help' 
-        ? await msg.reply(`Comandos disponíveis:\n${Object.keys(commands).sort((a,b) => a-b).join('\n')}`)
-        : command && await command(msg, client);
-    });
+    const { body } = msg;
+    const normalizedBody = body.split(" ")[0]?.toString().toLowerCase();
+    const command = commands[normalizedBody];
+
+    body === "!help"
+      ? await msg.reply(
+          `Comandos disponíveis:\n${Object.keys(commands)
+            .sort((a, b) => a - b)
+            .join("\n")}`
+        )
+      : command && (await command(msg, client));
+  });
 };
