d1a933b2879511b1ef60904a8bc68b0f4929e547
André Freitas <freitasandre38@gmail.com>
2024-04-21 08:50:21 -0300

    chore: removing comments from scrappings index.js

--- src/scrappings/index.js
+++ src/scrappings/index.js
@@ -7,10 +7,8 @@ async function makeScrapping() {
 
   const scrapingPromises = [fetchEditais(), fetchMenuRU(), fetchJobs()];
 
-  // Leia mais sobre allSettled 
   const results = await Promise.allSettled(scrapingPromises);
 
-  // Diferentemente do Promise.all, o allSettled armazena os resultados. Se necessário, crie uma função extra para validar.
   results.forEach((result, index) => {
     if (result.status === "fulfilled") {
       global.appContext[index === 0 ? "editais" : index === 1 ? "ruCardapio" : "jobs"] = result.value;
