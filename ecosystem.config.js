710f88f2f2a031e2ada8820ee5f19e562f5f8a89
jose cassio <josecassio2013@gmail.com>
2025-02-16 18:45:11 -0300

    setup: confgiure pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "bti_help_bot",
+      script: "./src/app.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
