3607a89982f65747027918bcddef98e0ce3503c9
JCassio <josecassio2013@gmail.com>
2025-09-02 18:58:31 -0300

    feat: link command

---
+++ src/commands/links.ts
@@ -0,0 +1,25 @@
+import { AnyMessageContent } from "baileys";
+import { Command } from "../types/command";
+
+const linksCommand: Command = {
+  name: "links",
+  description: "Retorna links relacionados à comunidade do BTI",
+  aliases: ["link", "comunidade", "grupo"],
+  privateRestricted: false,
+  loggable: true,
+  execute: async (
+    _sock,
+    _msg,
+    _args
+  ): Promise<AnyMessageContent | string | null | undefined> => {
+    return `
+        Aqui estão alguns links úteis relacionados à comunidade do BTI:
+        - *Comunidade do BTI*: https://chat.whatsapp.com/JdQxMkPs8ZYKg8kFPlXZ8U?mode=ems_copy_t 
+        - *Grupo de estudos*: https://chat.whatsapp.com/LYIeU3U0T8Z04njekhjalN
+        - *Portal IMD*: https://www.metropoledigital.ufrn.br/portal/
+
+    `;
+  },
+};
+
+export default linksCommand;
